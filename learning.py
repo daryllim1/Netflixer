a2fe2b4a9e8cd3599040ba288421ece2de5647ec
daryllim1 <daryllimkl@gmail.com>
2023-11-09 02:32:42 +0800

    t

--- learning.py
+++ learning.py
@@ -12,7 +12,9 @@ from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
-
+import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
+import seaborn as sns
 import warnings
 
 # Suppress the warning
@@ -28,7 +30,7 @@ rel_path = "netflix_titles.csv"
 abs_file_path = os.path.join(script_dir, rel_path)
 
 df = pd.read_csv(abs_file_path)
-
+data = pd.read_csv(abs_file_path)
 
 df = df[df['rating'].isin(['PG-13', 'TV-MA', 'PG', 'TV-14', 'TV-PG', 'TV-Y', 'R', 'TV-G', 'TV-Y7', 'G', 'NC-17', 'NR', 'TV-Y7-FV', 'UR'])]
 
@@ -131,7 +133,7 @@ country = [item.split(',')[0] for item in countries]
     
 print(country)
 print(age_categories)
-
+'''
 fig, ax = plt.subplots(figsize=(100,6))
 
 
@@ -146,9 +148,65 @@ plt.xticks(rotation=90)
 
 plt.tight_layout()
 plt.show()
+'''
 
 
+data= data['listed_in'].astype(str).apply(lambda s : s.replace('&',' ').replace(',', ' ').split()) 
+
+test = data
+mlb = MultiLabelBinarizer()
+res = pd.DataFrame(mlb.fit_transform(test), columns=mlb.classes_)
+corr = res.corr()
+mask = np.zeros_like(corr, dtype=bool)
+mask[np.triu_indices_from(mask)] = True
+f, ax = plt.subplots(figsize=(35, 34))
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,square=True, linewidths=.5, cbar_kws={"shrink": .5})
+plt.show()
 
 
+import networkx as nx
+
+stocks = corr.index.values
+cor_matrix = np.asmatrix(corr)
+G = nx.DiGraph(cor_matrix)
+G = nx.relabel_nodes(G,lambda x: stocks[x])
+G.edges(data=True)
+
+def create_corr_network(G, corr_direction, min_correlation):
+    H = G.copy()
+    for stock1, stock2, weight in G.edges(data=True):
+        if corr_direction == "positive":
+            if weight["weight"] <0 or weight["weight"] < min_correlation:
+                H.remove_edge(stock1, stock2)
+        else:
+            if weight["weight"] >=0 or weight["weight"] > min_correlation:
+                H.remove_edge(stock1, stock2)
+                
+    edges,weights = zip(*nx.get_edge_attributes(H,'weight').items())
+    weights = tuple([(1+abs(x))**2 for x in weights])
+    d = nx.degree(H)
+    nodelist, node_sizes = zip(*d)
+    positions=nx.circular_layout(H)
+    
+    plt.figure(figsize=(10,10), dpi=72)
+    
+    nx.draw_networkx_nodes(H,positions,node_color='#DA70D6',nodelist=nodelist,
+                           node_size=tuple([x**2 for x in node_sizes]),alpha=0.8)
+    
+    nx.draw_networkx_labels(H, positions, font_size=8, 
+                            font_family='sans-serif')
+    
+    if corr_direction == "positive": edge_colour = plt.cm.GnBu 
+    else: edge_colour = plt.cm.PuRd
+        
+    nx.draw_networkx_edges(H, positions, edgelist=edges,style='solid',
+                          width=weights, edge_color = weights, edge_cmap = edge_colour,
+                          edge_vmin = min(weights), edge_vmax=max(weights))
+    plt.axis('off')
+    plt.show() 
+    
+create_corr_network(G, 'positive', 0.3)
+create_corr_network(G, 'positive', -0.3)
 
 
