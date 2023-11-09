029a03fded2ea11a5df26a3c2aea4f2af155173c
daryllim1 <daryllimkl@gmail.com>
2023-11-10 00:55:25 +0800

    a

--- learning.py
+++ learning.py
@@ -16,6 +16,7 @@ import numpy as np
 from sklearn.preprocessing import MultiLabelBinarizer
 import seaborn as sns
 import warnings
+import networkx as nx
 
 # Suppress the warning
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -133,7 +134,7 @@ country = [item.split(',')[0] for item in countries]
     
 print(country)
 print(age_categories)
-
+'''
 fig, ax = plt.subplots(figsize=(100,6))
 
 
@@ -150,22 +151,20 @@ plt.tight_layout()
 plt.show()
 
 
+'''
 
-data= data['listed_in'].astype(str).apply(lambda s : s.replace('&',' ').replace(',', ' ').split()) 
+data = data['listed_in'].astype(str).apply(lambda s : s.replace('&',' ').replace(',', ' ').split()) 
 
 test = data
 mlb = MultiLabelBinarizer()
 res = pd.DataFrame(mlb.fit_transform(test), columns=mlb.classes_)
 corr = res.corr()
-mask = np.zeros_like(corr, dtype=bool)
-mask[np.triu_indices_from(mask)] = True
-f, ax = plt.subplots(figsize=(35, 34))
-cmap = sns.diverging_palette(220, 10, as_cmap=True)
-sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+# Use hierarchical clustering to cluster rows and columns
+clustered_corr = sns.clustermap(corr, figsize=(40, 40), method='average', cmap='coolwarm', annot=True, linewidths=.5,row_cluster=False, col_cluster=False,annot_kws={'size': 6, 'fontweight': 'bold', 'fontfamily': 'serif'})
+clustered_corr.ax_heatmap.set_xticklabels(clustered_corr.ax_heatmap.get_xticklabels(), fontsize=10)
+clustered_corr.ax_heatmap.set_yticklabels(clustered_corr.ax_heatmap.get_yticklabels(), fontsize=10)
 plt.show()
-
-
-import networkx as nx
 
 stocks = corr.index.values
 cor_matrix = np.asmatrix(corr)
@@ -173,40 +172,41 @@ G = nx.DiGraph(cor_matrix)
 G = nx.relabel_nodes(G,lambda x: stocks[x])
 G.edges(data=True)
 
-def create_corr_network(G, corr_direction, min_correlation):
+def create_force_directed_network(G, corr_direction, min_correlation):
     H = G.copy()
     for stock1, stock2, weight in G.edges(data=True):
         if corr_direction == "positive":
-            if weight["weight"] <0 or weight["weight"] < min_correlation:
+            if weight["weight"] < 0 or weight["weight"] < min_correlation:
                 H.remove_edge(stock1, stock2)
         else:
-            if weight["weight"] >=0 or weight["weight"] > min_correlation:
+            if weight["weight"] >= 0 or weight["weight"] > min_correlation:
                 H.remove_edge(stock1, stock2)
-                
-    edges,weights = zip(*nx.get_edge_attributes(H,'weight').items())
-    weights = tuple([(1+abs(x))**2 for x in weights])
+
+    edges, weights = zip(*nx.get_edge_attributes(H, 'weight').items())
+    weights = tuple([(1 + abs(x))**2 for x in weights])
     d = nx.degree(H)
     nodelist, node_sizes = zip(*d)
-    positions=nx.circular_layout(H)
-    
-    plt.figure(figsize=(10,10), dpi=72)
-    
-    nx.draw_networkx_nodes(H,positions,node_color='#DA70D6',nodelist=nodelist,
-                           node_size=tuple([x**2 for x in node_sizes]),alpha=0.8)
-    
-    nx.draw_networkx_labels(H, positions, font_size=8, 
-                            font_family='sans-serif')
-    
-    if corr_direction == "positive": edge_colour = plt.cm.GnBu 
-    else: edge_colour = plt.cm.PuRd
-        
-    nx.draw_networkx_edges(H, positions, edgelist=edges,style='solid',
-                          width=weights, edge_color = weights, edge_cmap = edge_colour,
-                          edge_vmin = min(weights), edge_vmax=max(weights))
-    plt.axis('off')
-    plt.show() 
-    
-create_corr_network(G, 'positive', 0.3)
-create_corr_network(G, 'positive', -0.3)
+    positions = nx.spring_layout(H, seed=42)  # Force-directed layout
 
+    plt.figure(figsize=(10, 10), dpi=72)
+
+    nx.draw_networkx_nodes(H, positions, node_color='#DA70D6', nodelist=nodelist,
+                           node_size=tuple([x**2 for x in node_sizes]), alpha=0.8)
+
+    nx.draw_networkx_labels(H, positions, font_size=8,
+                            font_family='sans-serif')
+
+    if corr_direction == "positive":
+        edge_colour = plt.cm.GnBu
+    else:
+        edge_colour = plt.cm.PuRd
+
+    nx.draw_networkx_edges(H, positions, edgelist=edges, style='solid',
+                           width=weights, edge_color=weights, edge_cmap=edge_colour,
+                           edge_vmin=min(weights), edge_vmax=max(weights))
+    plt.axis('off')
+    plt.show()
+
+create_force_directed_network(G, 'positive', 0.3)
+create_force_directed_network(G, 'positive', -0.3)
 
